bb79a993ce475a652dfa0792aba79d13098d7af5
Kyle Barry <kylesbarry@gmail.com>
2023-09-06 14:49:06 -0400

    added mongodb connection and registeruser functionality

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-const port = process.env.PORT || 8000;
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+const port = process.env.PORT || 8000;
 
 const app = express();
 
+connectDB();
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
