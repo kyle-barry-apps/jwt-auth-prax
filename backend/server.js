ddf422d24f3f342d01b967ad79b88b7d199e3f7a
Kyle Barry <kylesbarry@gmail.com>
2023-09-08 13:19:09 -0400

    added api and user slices in redux

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import morgan from "morgan";
+import cors from "cors";
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -14,6 +15,12 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(morgan("tiny"));
 
